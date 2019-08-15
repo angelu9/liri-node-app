b17a3b29cdb771e4301647ff122f055b104b1a51
angel m <angelmartinez@Angels-MacBook-Pro.local>
2019-08-14 21:27:47 -0400

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # liri-node-app
-Homework
+
+This app called liri, is a Language Interpretation and Recognization Interface. LIRI is similar to Iphone's SIRI, an Speech Interpretation and Recognization Interface. LIRI is a command line node app that takes in parameters and gives you back data. 
+
+## Instructions
+
+1. Clone Repo.
+2. Open liri-node-app folder.
+3. Create a keys.js file inside of the cloned folder.
+4. Get Spotify keys from https://developer.spotify.com/my-applications/#!/ to use for the Spotify API and save them inside of the keys.js file.
+5. Open the liri.js file and run node through terminal.
+6. Use this coomands to make th app work:
+
+node liri.js concert-this "artist/band name here"
+node liri.js spotify-this-song "song name here"
+node liri.js movie-this "movie name here"
+node liri.js do-what-it-says
+
+
+### Prerequisites
+
+To retrieve the data that will power this app, you'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs.
+
+1. Node-Spotify-API
+2. Axios
+3. You'll use Axios to grab data from the OMDB API and the Bands In Town API
+Moment
+4. DotEnv
+
+
+## Deployment
+
+Node.js
+
+
+## Authors
+
+* **Angel Martinez** - [angelu9](https://github.com/angelu9)
+
+
+## Acknowledgments
+
+* UCF Coding Bootcamp
+* YouTube
+* W3schools.com
+
+
+## Images
+
+![spotify-this-song](images/spotify-this.png)
+![movie-this](images/movie-this.png)
+![concert-this](images/concert-this.png)
+![do-what-it-says](images/dowhatitsays.png)
+
+
--- liri.js
+++ liri.js
@@ -1,9 +1,152 @@
 require("dotenv").config();
 
-var keys = reuire("./keys");
+var keys = require("./keys");
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
-
 var spotify = new Spotify(keys.spotify);
+
+
+
+var getArtist = function(artist) {
+    return artist.name;
+  };
+  
+ 
+  var spotifyThis = function(songName) {
+    if (songName === undefined) {
+      songName = "The Sign";
+    }
+  
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error: " + err);
+          return;
+        }
+  
+        var songs = data.tracks.items;
+  
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songs[i].artists.map(getArtist));
+          console.log("song name: " + songs[i].name);
+          console.log("preview song: " + songs[i].preview_url);
+          console.log("album: " + songs[i].album.name);
+          console.log("-----------------------------------");
+        }
+      }
+    );
+  };
+  
+
+  var movieThis= function(movieName) {
+    if (movieName === undefined) {
+      movieName = "Mr Nobody";
+    }
+  
+    var movieUrl =
+      "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  
+    axios.get(movieUrl).then(
+      function(response) {
+        var jsonData = response.data;
+  
+        console.log("Title: " + jsonData.Title);
+        console.log("Year: " + jsonData.Year);
+        console.log("Rated: " + jsonData.Rated);
+        console.log("IMDB Rating: " + jsonData.imdbRating);
+        console.log("Country: " + jsonData.Country);
+        console.log("Language: " + jsonData.Language);
+        console.log("Plot: " + jsonData.Plot);
+        console.log("Actors: " + jsonData.Actors);
+        console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      }
+    );
+  };
+
+  
+
+  var concertThis = function(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  
+    axios.get(queryURL).then(
+      function(response) {
+        var jsonData = response.data;
+  
+        if (!jsonData.length) {
+          console.log("No results found for " + artist);
+          return;
+        }
+  
+        console.log("Upcoming concerts for " + artist + ":");
+  
+        for (var i = 0; i < jsonData.length; i++) {
+          var show = jsonData[i];
+  
+          
+          console.log(
+            show.venue.city +
+              "," +
+              (show.venue.region || show.venue.country) +
+              " at " +
+              show.venue.name +
+              " " +
+              moment(show.datetime).format("MM/DD/YYYY")
+          );
+        }
+      }
+    );
+  };
+
+
+
+  var doThis = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      console.log(data);
+  
+      var dataArr = data.split(",");
+  
+      if (dataArr.length === 2) {
+        pick(dataArr[0], dataArr[1]);
+      } else if (dataArr.length === 1) {
+        pick(dataArr[0]);
+      }
+    });
+  };
+
+  
+  
+  var pick = function(caseData, functionData) {
+    switch (caseData) {
+    
+    case "spotify-this-song":
+      spotifyThis(functionData);
+      break;
+    case "movie-this":
+      movieThis(functionData);
+      break;
+    case "concert-this":
+      concertThis(functionData);
+      break;
+    case "do-what-it-says":
+      doThis();
+      break;
+    default:
+      console.log("Error, try again");
+    }
+  };
+  
+  
+ 
+  var startThis = function(firstArgument, secondArgument) {
+    pick(firstArgument, secondArgument);
+  };
+  
+  
+  startThis(process.argv[2], process.argv.slice(3).join(" "));
